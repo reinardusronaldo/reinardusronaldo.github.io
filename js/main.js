3c8db7349d21746195fddd261bb2acca65071e5b
aldojexa <repo@jexadev.com>
2024-02-20 19:43:57 +0700

    change ui

--- js/main.js
+++ js/main.js
@@ -118,15 +118,15 @@
 	/*----------------------------------------------------*/
 	/*	Modal Popup
 	------------------------------------------------------*/
-   $('.item-wrap a').magnificPopup({
+  //  $('.item-wrap a').magnificPopup({
 
-      type:'inline',
-      fixedContentPos: false,
-      removalDelay: 300,
-      showCloseBtn: false,
-      mainClass: 'mfp-fade'
+  //     type:'inline',
+  //     fixedContentPos: false,
+  //     removalDelay: 300,
+  //     showCloseBtn: false,
+  //     mainClass: 'mfp-fade'
 
-   });
+  //  });
 
    $(document).on('click', '.popup-modal-dismiss', function (e) {
    	e.preventDefault();
@@ -264,6 +264,19 @@
 
 	});
 
+	var swiper = new Swiper(".mySwiper", {
+		slidesPerView: 1,
+		grabCursor: true,
+		loop: true,
+		pagination: {
+			el: ".swiper-pagination",
+			clickable: true,
+		},
+		navigation: {
+			nextEl: ".swiper-button-next",
+			prevEl: ".swiper-button-prev",
+		},
+	});
 
  	/*----------------------------------------------------- */
   	/* Back to top
